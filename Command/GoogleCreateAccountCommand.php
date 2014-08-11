e0ddc191a0667e5884df9bab871939c8d0f10012
Roberto Mansfield <robertom@sas.upenn.edu>
2014-08-11 15:09:09 -0400

    Replace PersonInfo references with new bundle version

--- Command/GoogleCreateAccountCommand.php
+++ Command/GoogleCreateAccountCommand.php
@@ -8,7 +8,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\DependencyInjection\Exception\ServiceNotFoundException;
-use SAS\IRAD\GoogleAdminClientBundle\Service\PersonInfo;
+use SAS\IRAD\PersonInfoBundle\PersonInfo\PersonInfo;
 
 class GoogleCreateAccountCommand extends ContainerAwareCommand {
     
