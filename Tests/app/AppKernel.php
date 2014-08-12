887d28250f02bf6f6f5141fa4a694b357f8742d3
Roberto Mansfield <robertom@sas.upenn.edu>
2014-08-12 16:54:10 -0400

    Add symfony 2 command line environment for testing / devel

---
+++ Tests/app/AppKernel.php
@@ -0,0 +1,26 @@
+<?php
+
+use Symfony\Component\HttpKernel\Kernel;
+use Symfony\Component\Config\Loader\LoaderInterface;
+
+class AppKernel extends Kernel
+{
+    public function registerBundles()
+    {
+        $bundles = array(
+            new Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
+            new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),                
+            new SAS\IRAD\FileStorageBundle\FileStorageBundle(),
+            new SAS\IRAD\GmailAccountLogBundle\GmailAccountLogBundle(),
+            new SAS\IRAD\GmailOAuth2TokenBundle\GmailOAuth2TokenBundle(),
+            new SAS\IRAD\GoogleAdminClientBundle\GoogleAdminClientBundle(),
+         );
+
+        return $bundles;
+    }
+
+    public function registerContainerConfiguration(LoaderInterface $loader)
+    {
+        $loader->load(__DIR__.'/config/config_'.$this->getEnvironment().'.yml');
+    }
+}
